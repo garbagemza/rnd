1b952f5b7057cf61f3028f4d390cb75ac8c2f86f
garbagemza <garbagemza@gmail.com>
2023-05-13 18:15:20 -0300

    added xoshiro256** algorithm

---
+++ src/xoshiro256ss.c
@@ -0,0 +1,49 @@
+/**
+  * This is free and unencumbered software released into the public domain.
+  *
+  * Anyone is free to copy, modify, publish, use, compile, sell, or
+  * distribute this software, either in source code form or as a compiled
+  * binary, for any purpose, commercial or non-commercial, and by any
+  * means.
+  *
+  * In jurisdictions that recognize copyright laws, the author or authors
+  * of this software dedicate any and all copyright interest in the
+  * software to the public domain. We make this dedication for the benefit
+  * of the public at large and to the detriment of our heirs and
+  * successors. We intend this dedication to be an overt act of
+  * relinquishment in perpetuity of all present and future rights to this
+  * software under copyright law.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  * OTHER DEALINGS IN THE SOFTWARE.
+  *
+  * For more information, please refer to <https://unlicense.org>
+  **/
+
+#include "xoshiro256ss.h"
+
+uint64_t rol64(uint64_t x, uint8_t k) {
+  return (x << k) | (x >> (64 - k));
+}
+
+
+uint64_t rnd_xoshiro256ss(struct rnd_xoshiro256ss_state *state) {
+  uint64_t *s = state->s;
+  uint64_t const result = rol64(s[1]*5, 7) * 9;
+  uint64_t const t = s[1] << 17;
+
+  s[2] ^= s[0];
+  s[3] ^= s[1];
+  s[1] ^= s[2];
+  s[0] ^= s[3];
+  s[2] ^= t;
+  s[3] = rol64(s[3], 45);
+
+  return result;
+}
+
